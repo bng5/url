6311c711b0ad94de55170e71f1c57ef88cc998bd
Pablo Bangueses <pablo@bng5.net>
2014-09-30 00:15:23 -0300

    Protocolo y puerto predeterminados.

--- url.c
+++ url.c
@@ -13,17 +13,16 @@
 
 static char *str_hosttype[] = { "host ipv4", "host ipv6", "host domain", NULL };
 
-static char *strndup(const char *str, int n)
-{
-   char *dst;
-   if (!str) return NULL;
-   if (n < 0) n = strlen(str);
-   if (n == 0) return NULL;
-   if ((dst = (char *)malloc(n + 1)) == NULL)
-      return NULL;
-   memcpy(dst, str, n);
-   dst[n] = 0;
-   return dst;
+static char *strndup2(const char *str, int n) {
+    char *dst;
+    if (!str) return NULL;
+    if (n < 0) n = strlen(str);
+    if (n == 0) return NULL;
+    if ((dst = (char *)malloc(n + 1)) == NULL)
+        return NULL;
+    memcpy(dst, str, n);
+    dst[n] = 0;
+    return dst;
 }
 
 static int host_is_ipv4(char *str)
@@ -53,19 +52,19 @@ void parse_query(url_field_t *url, char *query)
          url->query = realloc(url->query, (url->query_num + 1) * sizeof(*url->query));
       else
          url->query = malloc(sizeof(*url->query));
-      url->query[url->query_num].name = strndup(query, chr - query);
+      url->query[url->query_num].name = strndup2(query, chr - query);
       query = chr + 1;
       chr = strchr(query, '&');
       if (chr)
       {
-         url->query[url->query_num].value = strndup(query, chr - query);
+         url->query[url->query_num].value = strndup2(query, chr - query);
          url->query_num++;
          query = chr + 1;
          chr = strchr(query, '=');
       }
       else
       {
-         url->query[url->query_num].value = strndup(query, -1);
+         url->query[url->query_num].value = strndup2(query, -1);
          url->query_num++;
          break;
       }
@@ -82,27 +81,29 @@ url_field_t *url_parse (const char *str)
    memset(url, 0, sizeof(url_field_t));
    if (str && str[0])
    {
-      url->href = strndup(str, -1);
+      url->href = strndup2(str, -1);
       pch = strchr(str, ':');   /* parse schema */
       if (pch && pch[1] == '/' && pch[2] == '/')
       {
-         url->schema = strndup(str, pch - str);
+         url->schema = strndup2(str, pch - str);
          str = pch + 3;
       }
-      else
-         goto __fail;
+      else {
+         url->schema = strndup2("http", 4);
+         //goto __fail;
+      }
       pch = strchr(str, '@');   /* parse user info */
       if (pch)
       {
          pch = strchr(str, ':');
          if (pch)
          {
-            url->username = strndup(str, pch - str);
+            url->username = strndup2(str, pch - str);
             str = pch + 1;
             pch = strchr(str, '@');
             if (pch)
             {
-               url->password = strndup(str, pch - str);
+               url->password = strndup2(str, pch - str);
                str = pch + 1;
             }
             else
@@ -117,7 +118,7 @@ url_field_t *url_parse (const char *str)
          pch = strchr(str, ']');
          if (pch)
          {
-            url->host = strndup(str, pch - str);
+            url->host = strndup2(str, pch - str);
             str = pch + 1;
             if (str[0] == ':')
             {
@@ -125,14 +126,22 @@ url_field_t *url_parse (const char *str)
                pch = strchr(str, '/');
                if (pch)
                {
-                  url->port = strndup(str, pch - str);
+                  url->port = strndup2(str, pch - str);
                   str = pch + 1;
                }
                else
                {
-                  url->port = strndup(str, -1);
+                  url->port = strndup2(str, -1);
                   str = str + strlen(str);
                }
+            }
+            else {
+                if(strcmp(url->schema, "http") == 0) {
+                    url->port = strndup2("80", 2);
+                }
+                else if(strcmp(url->schema, "https") == 0) {
+                    url->port = strndup2("443", 3);
+                }
             }
             url->host_type = HOST_IPV6;
          }
@@ -146,17 +155,17 @@ url_field_t *url_parse (const char *str)
          pch_slash = strchr(str, '/');
          if (pch && (!pch_slash || (pch_slash && pch<pch_slash)))
          {
-            url->host = strndup(str, pch - str);
+            url->host = strndup2(str, pch - str);
             str = pch + 1;
             pch = strchr(str, '/');
             if (pch)
             {
-               url->port = strndup(str, pch - str);
+               url->port = strndup2(str, pch - str);
                str = pch + 1;
             }
             else
             {
-               url->port = strndup(str, -1);
+               url->port = strndup2(str, -1);
                str = str + strlen(str);
             }
          }
@@ -165,34 +174,42 @@ url_field_t *url_parse (const char *str)
             pch = strchr(str, '/');
             if (pch)
             {
-               url->host = strndup(str, pch - str);
+               url->host = strndup2(str, pch - str);
                str = pch + 1;
             }
             else
             {
-               url->host = strndup(str, -1);
+               url->host = strndup2(str, -1);
                str = str + strlen(str);
             }
          }
          url->host_type = host_is_ipv4(url->host) ? HOST_IPV4 : HOST_DOMAIN;
+         if (!url->port) {
+             if(strcmp(url->schema, "http") == 0) {
+                 url->port = strndup2("80", 2);
+             }
+             else if(strcmp(url->schema, "https") == 0) {
+                 url->port = strndup2("443", 3);
+             }
+         }
       }
       if (str[0])               /* parse path, query and fragment */
       {
          pch = strchr(str, '?');
          if (pch)
          {
-            url->path = strndup(str, pch - str);
+            url->path = strndup2(str, pch - str);
             str = pch + 1;
             pch = strchr(str, '#');
             if (pch)
             {
-               query = strndup(str, pch - str);
+               query = strndup2(str, pch - str);
                str = pch + 1;
-               url->fragment = strndup(str, -1);
+               url->fragment = strndup2(str, -1);
             }
             else
             {
-               query = strndup(str, -1);
+               query = strndup2(str, -1);
                str = str + strlen(str);
             }
             parse_query(url, query);
@@ -203,14 +220,14 @@ url_field_t *url_parse (const char *str)
             pch = strchr(str, '#');
             if (pch)
             {
-               url->path = strndup(str, pch - str);
+               url->path = strndup2(str, pch - str);
                str = pch + 1;
-               url->fragment = strndup(str, -1);
+               url->fragment = strndup2(str, -1);
                str = str + strlen(str);
             }
             else
             {
-               url->path = strndup(str, -1);
+               url->path = strndup2(str, -1);
                str = str + strlen(str);
             }
          }
@@ -276,12 +293,12 @@ void url_field_print(url_field_t *url)
    if (url->fragment)
       fprintf(stdout, "  - fragment: '%s'\n", url->fragment);
 }
-#if 0
-int main()
+// # if 0
+int main(int argc, char *argv[])
 {
-   url_field_t *url = url_parse("schema://usr:pwd@localhost:port/path?a=b&c=d&e=f#foo");
+   url_field_t *url = url_parse(argv[1]);
    url_field_print(url);
    url_free(url);
 }
-#endif
+// # endif
 
